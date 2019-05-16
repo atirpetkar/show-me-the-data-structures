bdd49513b1ba3bdd0fa2b091e8a6c95df237c31e
clarence <clarence@you.co>
2019-05-16 09:27:17 +0800

    Updated problem 2

--- problem_2.py
+++ problem_2.py
@@ -22,19 +22,23 @@ def find_files(suffix, path):
     match_path_list = list()
 
     def find_files_recursive(path):
-        path_list = os.scandir(path)
+        try:
+            path_list = os.scandir(path)
 
-        # Process each file/folder under each path
-        for dir_entry in path_list:
-            # For file, match suffix and add to list
-            if dir_entry.is_file():
-                if dir_entry.path.endswith(suffix):
-                    match_path_list.append(dir_entry.path)
-                continue
+            # Process each file/folder under each path
+            for dir_entry in path_list:
+                # For file, match suffix and add to list
+                if dir_entry.is_file():
+                    if dir_entry.path.endswith(suffix):
+                        match_path_list.append(dir_entry.path)
+                    continue
 
-            # For sub-directories, call the function recursively for processing
-            if dir_entry.is_dir():
-                find_files_recursive(dir_entry.path)
+                # For sub-directories, call the function recursively for processing
+                if dir_entry.is_dir():
+                    find_files_recursive(dir_entry.path)
+        except OSError as oserr:
+            print(f"Error scanning folder: {oserr}")
+            return
 
     find_files_recursive(path)
 
@@ -51,6 +55,19 @@ def test_function(test_case):
         print("Fail")
 
 
+# Test case 1: testdir provided should produce the correct output
 test_case_1 = ['.c', './testdir', ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']]
 test_function(test_case_1)
 # Should output: ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
+
+
+# Test case 2: non-existence folder should return an empty list
+test_case_2 = ['.c', './non-exist-dir', []]
+test_function(test_case_2)
+# Should print a line of error and output: []
+
+
+# Test case 3: non-existence suffix should return empty list
+test_case_3 = ['.abc', './testdir', []]
+test_function(test_case_3)
+# Should output: []
