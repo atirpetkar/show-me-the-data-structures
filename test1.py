b174f4f8f48b5b72ae975d10496ba2c28c4cf520
clarence <clarence@you.co>
2019-05-22 16:52:11 +0800

    Add testing file

--- test1.py
+++ test1.py
@@ -1,17 +1,134 @@
-input_str = "The bird is the word"
+from collections import Counter
 
-char_freq_tuple_list = list()
 
-char_freq_tuple_list = [('T', 1), ('h', 2), ('e', 2), ('b', 1)]
+class HuffmanNode(object):
+    """
+    A class represent a node in Huffman tree
+    """
+    def __init__(self, data=None, node_type=None, frequency=None):
+        # A node can be a frequency node or char node (i.e. the character)
+        self.node_type = node_type
+        self.data = data
+        self.frequency = frequency
+        self.left = None
+        self.right = None
 
-for char_freq_tuple in char_freq_tuple_list:
-    print(char_freq_tuple[0])
-    print(char_freq_tuple[1])
+    def __str__(self):
+        return f"Node type: {self.node_type}, data: {self.data}, frequency: {self.frequency}"
 
-print("----------")
 
-char_freq_tuple_list.sort(key=lambda r: r[1])
+class HuffmanTree(object):
+    """
+    A class representing a Huffman tree
+    """
+    def __init__(self):
+        self.root = None
 
-for char_freq_tuple in char_freq_tuple_list:
-    print(char_freq_tuple[0])
-    print(char_freq_tuple[1])
+    def __repr__(self):
+        node_str_list = list()
+
+        def traverse(node):
+            if node is None:
+                return
+
+            node_str_list.append(str(node))
+
+            traverse(node.left)
+            traverse(node.right)
+
+        traverse(self.root)
+
+        output_str = '\n'.join(node_str_list)
+
+        return output_str
+
+
+def build_sub_tree_from_tuples(tuple_left, tuple_right):
+    """
+    Build a Huffman tree from 2 tuples
+    :param tuple_left:
+    :param tuple_right:
+    :return:
+    """
+
+    sub_tree = HuffmanTree()
+
+    # Check whether the tuple is a char or already merged tree
+    tuple_left_data = tuple_left[0]
+    tuple_left_freq = tuple_left[1]
+
+    if isinstance(tuple_left_data, str):
+        huff_node_left = HuffmanNode(data=tuple_left_data, node_type='character', frequency=tuple_left_freq)
+    else:
+        huff_node_left = HuffmanNode(data=tuple_left_data.root, node_type='frequency', frequency=tuple_left_freq)
+
+    # Check whether the tuple is a char or already merged tree
+    tuple_right_data = tuple_right[0]
+    tuple_right_freq = tuple_right[1]
+
+    if isinstance(tuple_right_data, str):
+        huff_node_right = HuffmanNode(data=tuple_right_data, node_type='character', frequency=tuple_right_freq)
+    else:
+        huff_node_right = HuffmanNode(data=tuple_right_data.root, node_type='frequency', frequency=tuple_right_freq)
+
+    # Construct a summary frequency node
+    freq_sum = tuple_left_freq + tuple_right_freq
+
+    summary_freq_node = HuffmanNode(node_type='frequency', frequency=freq_sum)
+    summary_freq_node.left = huff_node_left
+    summary_freq_node.right = huff_node_right
+    sub_tree.root = summary_freq_node
+
+    return sub_tree, freq_sum
+
+
+def build_huffman_tree(data):
+    """
+    Build a Huffman tree base on the data argument
+    :param data: data to build tree
+    :return: the Huffman tree
+    """
+    huffman_tree = HuffmanTree()
+
+    # Convert the input data into a list of tuples, sorted by frequency
+    print(f"Input data: {data}")
+
+    input_str_char_counter = Counter(input_str)
+
+    input_str_char_freq_tuple_list = list()
+
+    for key, value in input_str_char_counter.items():
+        input_str_char_freq_tuple_list.append((key, value))
+
+    # Sort tuple by char frequency
+    input_str_char_freq_tuple_list.sort(key=lambda r: r[1])
+
+    print(input_str_char_freq_tuple_list)
+
+    while len(input_str_char_freq_tuple_list) > 1:
+        # Retrieve the first 2 tuples with lowest frequencies
+        char_freq_tuple_left = input_str_char_freq_tuple_list.pop(0)
+        # print(f"char_freq_tuple_left: {char_freq_tuple_left}")
+
+        char_freq_tuple_right = input_str_char_freq_tuple_list.pop(0)
+        # print(f"char_freq_tuple_right: {char_freq_tuple_right}")
+
+        # Build a subtree from the list
+        sub_tree, freq_sum = build_sub_tree_from_tuples(char_freq_tuple_left, char_freq_tuple_right)
+
+        # Add merged tree into tuple list
+        input_str_char_freq_tuple_list.append((sub_tree, freq_sum))
+
+        # Sort the list again
+        input_str_char_freq_tuple_list.sort(key=lambda r: r[1])
+
+    huffman_tree = input_str_char_freq_tuple_list[0]
+
+    return huffman_tree
+
+
+# input_str = "The bird is the word"
+input_str = "aabc"
+
+huffman_tree = build_huffman_tree(input_str)
+print(huffman_tree)
